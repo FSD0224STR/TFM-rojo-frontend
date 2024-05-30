37209b0a09c0598a86233fb3d2046b99743a8d43
syunay <syyunayy@gmail.com>
2024-05-30 20:45:41 +0200

    Avanzado userDetails
    Agregado ant description design

--- src/components/users/userDetails.jsx
+++ src/components/users/userDetails.jsx
@@ -1,43 +1,109 @@
-import { Card } from 'antd';
-import { useContext, useState } from "react";
-import { useParams } from "react-router-dom";
-import { AuthContext } from "../../contexts/authContext.jsx";
+import React from 'react';
+import { Badge, Descriptions } from 'antd';
+import { useContext, useState } from 'react';
+import {AuthContext } from '../../contexts/authContext.jsx';
 
-function UserDetails() {
- 
-  const {searchedUser} = useContext(AuthContext)
- 
+function userDetails() {
+  
+  const { searchedUser } = useContext(AuthContext);
+
+  const items = [
+    {
+      key: '1',
+      label: 'Nombre',
+      children: searchedUser?.name,
+    },
+    {
+      key: '2',
+      label: 'Apellido',
+      children: searchedUser?.lastName,
+    },
+    {
+      key: '3',
+      label: 'Email',
+      children: searchedUser?.email,
+    },
+    {
+      key: '4',
+      label: 'Country',
+      children: searchedUser?.country,
+    },
+    {
+      key: '5',
+      label: 'Province',
+      children: searchedUser?.province,
+      span: 2,
+    },
+    {
+      key: '6',
+      label: 'Es cliente',
+      children: <Badge status="processing" text="Sí" />,
+      span: 3,
+    },
+    {
+      key: '7',
+      label: 'Fecha de nacimiento',
+      children: searchedUser?.birthDay,
+    },
+    {
+      key: '8',
+      label: 'Rol',
+      children: searchedUser?.role,
+      span: 2,
+    },
+    {
+      key: '9',
+      label: 'Dni',
+      children: searchedUser?.dni,
+    },
+    {
+      key: '10',
+      label: 'Billing',
+      children: (
+        <>
+          Factura 1...
+          <br />
+          Factura 2...
+          <br />
+          Factura 3...
+          <br />
+          Factura 4...
+          <br />
+          Factura 5...
+          <br />
+          Factura 6...
+          <br />
+          Factura 7...
+          <br />
+          Factura 8...
+          <br />
+          Factura 9...
+          <br />
+          Factura 10...
+        </>
+      ),
+    },
+  ];
 
   return (
     <>
-      <Card title={`User Detail for user ${searchedUser?.name}`}>
-        <div>
-          <img src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${searchedUser?.id}`} alt="" />
-        </div>
-        <div>
-          <h3>User Data</h3>
-          <div>
-            <p>Dni: {searchedUser?.dni}</p>
-            <p>Name: {searchedUser?.name}</p>
-            <p>Last Name: {searchedUser?.lastName}</p>
-            <p>Email: {searchedUser?.email}</p>
-            <p>Country: {searchedUser?.country}</p>
-            <p>Province: {searchedUser?.province}</p>
-            <p>Birth Day: {searchedUser?.birthDay}</p>
-            <p>Role: {searchedUser?.role}</p>
-          </div>
-        </div>
-        <div>
-          <h3>Facturas del paciente</h3>
-          <div>
-            <p>Factura 1</p>
-            <p>Factura 2</p>
-            <p>Factura 3</p>
-          </div>
-        </div>
-      </Card>
+      <div>
+        <img src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${searchedUser?._id}`} alt='avatar' style={{ width: '250px', height: '250px', borderRadius: '50%', marginBottom: '5em', marginTop: '1em', border: '2px solid #fff', boxShadow: '0 0 10px #fff', translate: '-50%', position: 'absolute', top: '0', left: '20%', marginBottom: '40px' }} />
+        <br />
+      </div>
+      <div>
+        <Descriptions column={3} size="big" style={{ width: '100%', marginTop: '5em'}}>
+          {items.map((item) => (
+            <Descriptions.Item label={item.label}>
+              {item.children}
+            </Descriptions.Item>
+          ))}
+        </Descriptions>
+      </div>
     </>
+    
   );
 }
 
-export default UserDetails;
+
+export default userDetails;

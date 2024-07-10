a0213cb34cf8054b157f40d1f462981c5e9a8082
dagamanja <70232129+dagamanja@users.noreply.github.com>
2024-07-10 12:41:44 -0500

    Updated with all changes

--- src/components/users/userDetails.jsx
+++ src/components/users/userDetails.jsx
@@ -4,8 +4,8 @@ import { AuthContext } from "../../contexts/authContext.jsx";
 import FloatingEmailForm from "./emailForm.jsx";
 import dayjs from "dayjs";
 import { TableResume } from "../TableResume/TableResume.jsx";
-import React from 'react';
-import { Badge, Descriptions } from 'antd';
+import React from "react";
+import { Badge, Descriptions } from "antd";
 
 function UserDetails() {
   const [showEmailForm, setShowEmailForm] = useState(false);
@@ -53,27 +53,60 @@ function UserDetails() {
 
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'row', padding: '20px', justifyContent: 'space-around', width: '100%' }}>
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '20px' }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: "20px",
+          justifyContent: "space-around",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginRight: "20px",
+          }}
+        >
           <h1>{searchedUser?.name + " " + searchedUser?.lastName}</h1>
-          <div style={{ marginBottom: '20px' }}>
-            <img src={
-              searchedUser?.fileUrlLink !== undefined
-                ? searchedUser?.fileUrlLink
-                : `https://api.dicebear.com/7.x/miniavs/svg?seed=${searchedUser?._id}`
-            } alt="User" style={{ width: '300px', height: '300px', borderRadius: '50%' }} />
+          <div style={{ marginBottom: "20px" }}>
+            <img
+              src={
+                searchedUser?.fileUrlLink !== undefined
+                  ? searchedUser?.fileUrlLink
+                  : `https://api.dicebear.com/7.x/miniavs/svg?seed=${searchedUser?._id}`
+              }
+              alt="User"
+              style={{ width: "300px", height: "300px", borderRadius: "50%" }}
+            />
           </div>
           <div>
             <button
-                onClick={() => setShowEmailForm(!showEmailForm)}
-                style={{ marginBottom: '20px', backgroundColor: '#1890ff', color: 'white', border: 'none', padding: '10px', borderRadius: '50%', cursor: 'pointer'}}
-              >
-                <MailOutlined />
-              </button>
-              <FloatingEmailForm isVisible={showEmailForm} onClose={() => setShowEmailForm(false)} emailDefault={searchedUser.email} />
+              onClick={() => setShowEmailForm(!showEmailForm)}
+              style={{
+                marginBottom: "20px",
+                backgroundColor: "#1890ff",
+                color: "white",
+                border: "none",
+                padding: "10px",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }}
+            >
+              <MailOutlined />
+            </button>
+            <FloatingEmailForm
+              isVisible={showEmailForm}
+              onClose={() => setShowEmailForm(false)}
+              emailDefault={searchedUser.email}
+            />
           </div>
           <Descriptions title={""} layout="vertical" bordered>
-            {items.map(item => (
+            {items.map((item) => (
               <Descriptions.Item label={item.label} key={item.key}>
                 {item.children}
               </Descriptions.Item>
@@ -81,14 +114,22 @@ function UserDetails() {
           </Descriptions>
         </div>
 
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '20px' }}>
-          <div style={{ width: '100%', marginBottom: '20px' }}>
-            <h2 style={{ textAlign: 'center' }}>Bills</h2>
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginLeft: "20px",
+          }}
+        >
+          <div style={{ width: "100%", marginBottom: "20px" }}>
+            <h2 style={{ textAlign: "center" }}>Bills</h2>
             <TableResume searchid={searchedUser?._id} type="bill" />
           </div>
-  
-          <div style={{ width: '100%' }}>
-            <h2 style={{ textAlign: 'center' }}>Appointments</h2>
+
+          <div style={{ width: "100%" }}>
+            <h2 style={{ textAlign: "center" }}>Appointments</h2>
             <TableResume searchid={searchedUser?._id} type="dates" />
           </div>
         </div>

322c22cd52f170310525ea7769f92421be74f732
aliciagar8 <aliciagar8@gmail.com>
2024-07-18 20:29:38 +0200

    details

--- src/components/CreateBills/Bill.jsx
+++ src/components/CreateBills/Bill.jsx
@@ -62,27 +62,28 @@ export const Bill = ({ bill }) => {
       {patientName !== undefined && (
         <Space direction="vertical" size={30}>
           <Card
-            ///        title={`Bill Nº${bill?.billNumber}`}
+            
             style={{
               width: 500,
             }}
           >
             <div>
               <img
-                style={{ width: 70 }}
+                style={{ width: 70,  }}
                 src="MolarisLogo-removebg-preview.png"
                 alt="Logo"
               />
               <div>
-              <p>BILL Nº {bill?.billNumber} </p>
-              {dayjs(bill?.date).format("DD-MM-YYYY")}</div>
+              <strong>BILL Nº {bill?.billNumber} </strong> <br></br>
+              <strong>{dayjs(bill?.date).format("DD-MM-YYYY")}</strong>
+              </div>
             </div>
             <div className="empresa">
               <Empresa data={dataEmpresa} />
             </div>
 
-            <div>
-              <strong>Datos paciente:</strong>
+            <div style={{ lineHeight: 0.5 }}>
+             <strong>Patient Data:</strong>
               <p>{patientName} </p>
               <p> {bill?.dni} </p>
               <p> {bill?.adress} </p>
